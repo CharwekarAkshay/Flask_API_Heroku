e498b0baeb4c5fe4a70c17f2aa10392949c7664e
CharwekarAkshay <CharwekarAkshay@users.noreply.github.com>
2019-11-09 17:05:45 +0530

    Trying to add CORS

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+# For CORS 
+from flask_cors import CORS
+
 app = Flask(__name__)
+# This will enable request from each origin
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Item that has been change but not added in database. SQL_Alchemy has advance tracker that is why we are turning it off
 app.secret_key = 'This is my secret key please don\'t share it '
